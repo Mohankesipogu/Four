c0a168c9eb1a78d58174d61432404e9457e10c26
Dell <Dell@Dell-PC>
2022-03-12 21:14:42 +0530

    Five second time

---
+++ Object Repository/OrangeHRM/Page_OrangeHRM HR Software  Free  Open Sour_43e816/a_Book a Free Demo.rs
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_Book a Free Demo</name>
+   <tag></tag>
+   <elementGuidId>0ba6fb74-08da-4042-9c10-1068051546fb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='header-navbar']/ul[3]/li[2]/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/hris-hr-software-demo/</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>btn-orange trial-btn pulse </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Book a Free Demo</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;header-navbar&quot;)/ul[@class=&quot;nav navbar-nav navbar-right extended-nav&quot;]/li[@class=&quot;ex-nav&quot;]/a[@class=&quot;btn-orange trial-btn pulse&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='header-navbar']/ul[3]/li[2]/a</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Book a Free Demo')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Contact Sales'])[1]/following::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Careers'])[2]/following::a[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='EN'])[1]/preceding::a[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='ES'])[1]/preceding::a[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Book a Free Demo']/parent::*</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/hris-hr-software-demo/')]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//ul[3]/li[2]/a</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@href = '/hris-hr-software-demo/' and (text() = 'Book a Free Demo' or . = 'Book a Free Demo')]</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/OrangeHRM/Page_Sign Up for a Free HR Software Demo  O_589e71/select_- Select Country -AfghanistanAlbania_67af3b.rs
@@ -0,0 +1,1536 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_- Select Country -AfghanistanAlbania_67af3b</name>
+   <tag></tag>
+   <elementGuidId>86711274-31f5-4087-8c1d-0a0b58329261</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#Form_submitForm_Country</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id='Form_submitForm_Country']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>Country</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>dropdown countries fleft required</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>Form_submitForm_Country</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>required</name>
+      <type>Main</type>
+      <value>required</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-required</name>
+      <type>Main</type>
+      <value>true</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tabindex</name>
+      <type>Main</type>
+      <value>5</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+
+	- Select Country -
+
+	Afghanistan
+
+	Albania
+
+	Algeria
+
+	American Samoa
+
+	Andorra
+
+	Angola
+
+	Anguilla
+
+	Antigua and Barbuda
+
+	Argentina
+
+	Armenia
+
+	Aruba
+
+	Australia
+
+	Austria
+
+	Azerbaijan
+
+	Bahamas
+
+	Bahrain
+
+	Bangladesh
+
+	Barbados
+
+	Belarus
+
+	Belgium
+
+	Belize
+
+	Benin
+
+	Bermuda
+
+	Bhutan
+
+	Bolivia
+
+	Bosnia and Herzegowina
+
+	Botswana
+
+	Bouvet Island
+
+	Brazil
+
+	Brunei Darussalam
+
+	Bulgaria
+
+	Burkina Faso
+
+	Burundi
+
+	Cambodia
+
+	Cameroon
+
+	Canada
+
+	Cape Verde
+
+	Cayman Islands
+
+	Central African Republic
+
+	Chad
+
+	Chile
+
+	China
+
+	Christmas Island
+
+	Cocos (Keeling) Islands
+
+	Colombia
+
+	Comoros
+
+	Congo
+
+	Cook Islands
+
+	Costa Rica
+
+	Cote D'Ivoire
+
+	Croatia
+
+	Cuba
+
+	Cyprus
+
+	Czech Republic
+
+	Denmark
+
+	Djibouti
+
+	Dominica
+
+	Dominican Republic
+
+	East Timor
+
+	Ecuador
+
+	Egypt
+
+	El Salvador
+
+	Equatorial Guinea
+
+	Eritrea
+
+	Estonia
+
+	Ethiopia
+
+	Falkland Islands (Malvinas)
+
+	Faroe Islands
+
+	Fiji
+
+	Finland
+
+	France
+
+	French Guiana
+
+	French Polynesia
+
+	French Southern Territories
+
+	Gabon
+
+	Gambia
+
+	Georgia
+
+	Germany
+
+	Ghana
+
+	Gibraltar
+
+	Greece
+
+	Greenland
+
+	Grenada
+
+	Guadeloupe
+
+	Guam
+
+	Guatemala
+
+	Guinea
+
+	Guinea-bissau
+
+	Guyana
+
+	Haiti
+
+	Heard and Mc Donald Islands
+
+	Honduras
+
+	Hong Kong
+
+	Hungary
+
+	Iceland
+
+	India
+
+	Indonesia
+
+	Iran
+
+	Iraq
+
+	Ireland
+
+	Israel
+
+	Italy
+
+	Jamaica
+
+	Japan
+
+	Jordan
+
+	Kazakhstan
+
+	Kenya
+
+	Kiribati
+
+	North Korea
+
+	South Korea
+
+	Kuwait
+
+	Kyrgyzstan
+
+	Laos
+
+	Latvia
+
+	Lebanon
+
+	Lesotho
+
+	Liberia
+
+	Libya
+
+	Liechtenstein
+
+	Lithuania
+
+	Luxembourg
+
+	Macau
+
+	Macedonia
+
+	Madagascar
+
+	Malawi
+
+	Malaysia
+
+	Maldives
+
+	Mali
+
+	Malta
+
+	Marshall Islands
+
+	Martinique
+
+	Mauritania
+
+	Mauritius
+
+	Mayotte
+
+	Mexico
+
+	Moldova
+
+	Monaco
+
+	Mongolia
+
+	Morocco
+
+	Mozambique
+
+	Myanmar
+
+	Namibia
+
+	Nauru
+
+	Nepal
+
+	Netherlands
+
+	Netherlands Antilles
+
+	New Caledonia
+
+	New Zealand
+
+	Nicaragua
+
+	Niger
+
+	Nigeria
+
+	Niue
+
+	Norfolk Island
+
+	Northern Mariana Islands
+
+	Norway
+
+	Oman
+
+	Pakistan
+
+	Palau
+
+	Panama
+
+	Papua New Guinea
+
+	Paraguay
+
+	Peru
+
+	Philippines
+
+	Poland
+
+	Portugal
+
+	Puerto Rico
+
+	Qatar
+
+	Reunion
+
+	Romania
+
+	Russian Federation
+
+	Rwanda
+
+	St Kitts and Nevis
+
+	St Lucia
+
+	St Vincent and the Grenadines
+
+	Samoa
+
+	San Marino
+
+	Sao Tome and Principe
+
+	Saudi Arabia
+
+	Senegal
+
+	Seychelles
+
+	Sierra Leone
+
+	Singapore
+
+	Slovakia
+
+	Slovenia
+
+	Solomon Islands
+
+	Somalia
+
+	South Africa
+
+	South Georgia
+
+	Spain
+
+	Sri Lanka
+
+	Sudan
+
+	Suriname
+
+	Swaziland
+
+	Sweden
+
+	Switzerland
+
+	Syrian Arab Republic
+
+	Taiwan
+
+	Tajikistan
+
+	Tanzania
+
+	Thailand
+
+	Togo
+
+	Tokelau
+
+	Tonga
+
+	Trinidad and Tobago
+
+	Tunisia
+
+	Turkey
+
+	Turkmenistan
+
+	Turks and Caicos Islands
+
+	Tuvalu
+
+	Uganda
+
+	Ukraine
+
+	United Arab Emirates
+
+	United Kingdom
+
+	United States
+
+	Uruguay
+
+	Uzbekistan
+
+	Vanuatu
+
+	Venezuela
+
+	Vietnam
+
+	Virgin Islands
+
+	Western Sahara
+
+	Yemen
+
+	Yugoslavia
+
+	Zambia
+
+	Zimbabwe
+
+	South Sandwich Islands
+
+	St Helena
+
+	St Pierre and Miquelon
+
+	Vatican City
+
+	Wallis and Futuna Islands
+
+	Zaire
+
+</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;Form_submitForm_Country&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@id='Form_submitForm_Country']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='Form_submitForm_Country_Holder']/div/select</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Country'])[1]/following::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Phone Number'])[1]/following::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Privacy Policy'])[1]/preceding::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Why OrangeHRM Will Rock Your World'])[1]/preceding::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//select</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@name = 'Country' and @id = 'Form_submitForm_Country' and (text() = concat(&quot;
+
+	- Select Country -
+
+	Afghanistan
+
+	Albania
+
+	Algeria
+
+	American Samoa
+
+	Andorra
+
+	Angola
+
+	Anguilla
+
+	Antigua and Barbuda
+
+	Argentina
+
+	Armenia
+
+	Aruba
+
+	Australia
+
+	Austria
+
+	Azerbaijan
+
+	Bahamas
+
+	Bahrain
+
+	Bangladesh
+
+	Barbados
+
+	Belarus
+
+	Belgium
+
+	Belize
+
+	Benin
+
+	Bermuda
+
+	Bhutan
+
+	Bolivia
+
+	Bosnia and Herzegowina
+
+	Botswana
+
+	Bouvet Island
+
+	Brazil
+
+	Brunei Darussalam
+
+	Bulgaria
+
+	Burkina Faso
+
+	Burundi
+
+	Cambodia
+
+	Cameroon
+
+	Canada
+
+	Cape Verde
+
+	Cayman Islands
+
+	Central African Republic
+
+	Chad
+
+	Chile
+
+	China
+
+	Christmas Island
+
+	Cocos (Keeling) Islands
+
+	Colombia
+
+	Comoros
+
+	Congo
+
+	Cook Islands
+
+	Costa Rica
+
+	Cote D&quot; , &quot;'&quot; , &quot;Ivoire
+
+	Croatia
+
+	Cuba
+
+	Cyprus
+
+	Czech Republic
+
+	Denmark
+
+	Djibouti
+
+	Dominica
+
+	Dominican Republic
+
+	East Timor
+
+	Ecuador
+
+	Egypt
+
+	El Salvador
+
+	Equatorial Guinea
+
+	Eritrea
+
+	Estonia
+
+	Ethiopia
+
+	Falkland Islands (Malvinas)
+
+	Faroe Islands
+
+	Fiji
+
+	Finland
+
+	France
+
+	French Guiana
+
+	French Polynesia
+
+	French Southern Territories
+
+	Gabon
+
+	Gambia
+
+	Georgia
+
+	Germany
+
+	Ghana
+
+	Gibraltar
+
+	Greece
+
+	Greenland
+
+	Grenada
+
+	Guadeloupe
+
+	Guam
+
+	Guatemala
+
+	Guinea
+
+	Guinea-bissau
+
+	Guyana
+
+	Haiti
+
+	Heard and Mc Donald Islands
+
+	Honduras
+
+	Hong Kong
+
+	Hungary
+
+	Iceland
+
+	India
+
+	Indonesia
+
+	Iran
+
+	Iraq
+
+	Ireland
+
+	Israel
+
+	Italy
+
+	Jamaica
+
+	Japan
+
+	Jordan
+
+	Kazakhstan
+
+	Kenya
+
+	Kiribati
+
+	North Korea
+
+	South Korea
+
+	Kuwait
+
+	Kyrgyzstan
+
+	Laos
+
+	Latvia
+
+	Lebanon
+
+	Lesotho
+
+	Liberia
+
+	Libya
+
+	Liechtenstein
+
+	Lithuania
+
+	Luxembourg
+
+	Macau
+
+	Macedonia
+
+	Madagascar
+
+	Malawi
+
+	Malaysia
+
+	Maldives
+
+	Mali
+
+	Malta
+
+	Marshall Islands
+
+	Martinique
+
+	Mauritania
+
+	Mauritius
+
+	Mayotte
+
+	Mexico
+
+	Moldova
+
+	Monaco
+
+	Mongolia
+
+	Morocco
+
+	Mozambique
+
+	Myanmar
+
+	Namibia
+
+	Nauru
+
+	Nepal
+
+	Netherlands
+
+	Netherlands Antilles
+
+	New Caledonia
+
+	New Zealand
+
+	Nicaragua
+
+	Niger
+
+	Nigeria
+
+	Niue
+
+	Norfolk Island
+
+	Northern Mariana Islands
+
+	Norway
+
+	Oman
+
+	Pakistan
+
+	Palau
+
+	Panama
+
+	Papua New Guinea
+
+	Paraguay
+
+	Peru
+
+	Philippines
+
+	Poland
+
+	Portugal
+
+	Puerto Rico
+
+	Qatar
+
+	Reunion
+
+	Romania
+
+	Russian Federation
+
+	Rwanda
+
+	St Kitts and Nevis
+
+	St Lucia
+
+	St Vincent and the Grenadines
+
+	Samoa
+
+	San Marino
+
+	Sao Tome and Principe
+
+	Saudi Arabia
+
+	Senegal
+
+	Seychelles
+
+	Sierra Leone
+
+	Singapore
+
+	Slovakia
+
+	Slovenia
+
+	Solomon Islands
+
+	Somalia
+
+	South Africa
+
+	South Georgia
+
+	Spain
+
+	Sri Lanka
+
+	Sudan
+
+	Suriname
+
+	Swaziland
+
+	Sweden
+
+	Switzerland
+
+	Syrian Arab Republic
+
+	Taiwan
+
+	Tajikistan
+
+	Tanzania
+
+	Thailand
+
+	Togo
+
+	Tokelau
+
+	Tonga
+
+	Trinidad and Tobago
+
+	Tunisia
+
+	Turkey
+
+	Turkmenistan
+
+	Turks and Caicos Islands
+
+	Tuvalu
+
+	Uganda
+
+	Ukraine
+
+	United Arab Emirates
+
+	United Kingdom
+
+	United States
+
+	Uruguay
+
+	Uzbekistan
+
+	Vanuatu
+
+	Venezuela
+
+	Vietnam
+
+	Virgin Islands
+
+	Western Sahara
+
+	Yemen
+
+	Yugoslavia
+
+	Zambia
+
+	Zimbabwe
+
+	South Sandwich Islands
+
+	St Helena
+
+	St Pierre and Miquelon
+
+	Vatican City
+
+	Wallis and Futuna Islands
+
+	Zaire
+
+&quot;) or . = concat(&quot;
+
+	- Select Country -
+
+	Afghanistan
+
+	Albania
+
+	Algeria
+
+	American Samoa
+
+	Andorra
+
+	Angola
+
+	Anguilla
+
+	Antigua and Barbuda
+
+	Argentina
+
+	Armenia
+
+	Aruba
+
+	Australia
+
+	Austria
+
+	Azerbaijan
+
+	Bahamas
+
+	Bahrain
+
+	Bangladesh
+
+	Barbados
+
+	Belarus
+
+	Belgium
+
+	Belize
+
+	Benin
+
+	Bermuda
+
+	Bhutan
+
+	Bolivia
+
+	Bosnia and Herzegowina
+
+	Botswana
+
+	Bouvet Island
+
+	Brazil
+
+	Brunei Darussalam
+
+	Bulgaria
+
+	Burkina Faso
+
+	Burundi
+
+	Cambodia
+
+	Cameroon
+
+	Canada
+
+	Cape Verde
+
+	Cayman Islands
+
+	Central African Republic
+
+	Chad
+
+	Chile
+
+	China
+
+	Christmas Island
+
+	Cocos (Keeling) Islands
+
+	Colombia
+
+	Comoros
+
+	Congo
+
+	Cook Islands
+
+	Costa Rica
+
+	Cote D&quot; , &quot;'&quot; , &quot;Ivoire
+
+	Croatia
+
+	Cuba
+
+	Cyprus
+
+	Czech Republic
+
+	Denmark
+
+	Djibouti
+
+	Dominica
+
+	Dominican Republic
+
+	East Timor
+
+	Ecuador
+
+	Egypt
+
+	El Salvador
+
+	Equatorial Guinea
+
+	Eritrea
+
+	Estonia
+
+	Ethiopia
+
+	Falkland Islands (Malvinas)
+
+	Faroe Islands
+
+	Fiji
+
+	Finland
+
+	France
+
+	French Guiana
+
+	French Polynesia
+
+	French Southern Territories
+
+	Gabon
+
+	Gambia
+
+	Georgia
+
+	Germany
+
+	Ghana
+
+	Gibraltar
+
+	Greece
+
+	Greenland
+
+	Grenada
+
+	Guadeloupe
+
+	Guam
+
+	Guatemala
+
+	Guinea
+
+	Guinea-bissau
+
+	Guyana
+
+	Haiti
+
+	Heard and Mc Donald Islands
+
+	Honduras
+
+	Hong Kong
+
+	Hungary
+
+	Iceland
+
+	India
+
+	Indonesia
+
+	Iran
+
+	Iraq
+
+	Ireland
+
+	Israel
+
+	Italy
+
+	Jamaica
+
+	Japan
+
+	Jordan
+
+	Kazakhstan
+
+	Kenya
+
+	Kiribati
+
+	North Korea
+
+	South Korea
+
+	Kuwait
+
+	Kyrgyzstan
+
+	Laos
+
+	Latvia
+
+	Lebanon
+
+	Lesotho
+
+	Liberia
+
+	Libya
+
+	Liechtenstein
+
+	Lithuania
+
+	Luxembourg
+
+	Macau
+
+	Macedonia
+
+	Madagascar
+
+	Malawi
+
+	Malaysia
+
+	Maldives
+
+	Mali
+
+	Malta
+
+	Marshall Islands
+
+	Martinique
+
+	Mauritania
+
+	Mauritius
+
+	Mayotte
+
+	Mexico
+
+	Moldova
+
+	Monaco
+
+	Mongolia
+
+	Morocco
+
+	Mozambique
+
+	Myanmar
+
+	Namibia
+
+	Nauru
+
+	Nepal
+
+	Netherlands
+
+	Netherlands Antilles
+
+	New Caledonia
+
+	New Zealand
+
+	Nicaragua
+
+	Niger
+
+	Nigeria
+
+	Niue
+
+	Norfolk Island
+
+	Northern Mariana Islands
+
+	Norway
+
+	Oman
+
+	Pakistan
+
+	Palau
+
+	Panama
+
+	Papua New Guinea
+
+	Paraguay
+
+	Peru
+
+	Philippines
+
+	Poland
+
+	Portugal
+
+	Puerto Rico
+
+	Qatar
+
+	Reunion
+
+	Romania
+
+	Russian Federation
+
+	Rwanda
+
+	St Kitts and Nevis
+
+	St Lucia
+
+	St Vincent and the Grenadines
+
+	Samoa
+
+	San Marino
+
+	Sao Tome and Principe
+
+	Saudi Arabia
+
+	Senegal
+
+	Seychelles
+
+	Sierra Leone
+
+	Singapore
+
+	Slovakia
+
+	Slovenia
+
+	Solomon Islands
+
+	Somalia
+
+	South Africa
+
+	South Georgia
+
+	Spain
+
+	Sri Lanka
+
+	Sudan
+
+	Suriname
+
+	Swaziland
+
+	Sweden
+
+	Switzerland
+
+	Syrian Arab Republic
+
+	Taiwan
+
+	Tajikistan
+
+	Tanzania
+
+	Thailand
+
+	Togo
+
+	Tokelau
+
+	Tonga
+
+	Trinidad and Tobago
+
+	Tunisia
+
+	Turkey
+
+	Turkmenistan
+
+	Turks and Caicos Islands
+
+	Tuvalu
+
+	Uganda
+
+	Ukraine
+
+	United Arab Emirates
+
+	United Kingdom
+
+	United States
+
+	Uruguay
+
+	Uzbekistan
+
+	Vanuatu
+
+	Venezuela
+
+	Vietnam
+
+	Virgin Islands
+
+	Western Sahara
+
+	Yemen
+
+	Yugoslavia
+
+	Zambia
+
+	Zimbabwe
+
+	South Sandwich Islands
+
+	St Helena
+
+	St Pierre and Miquelon
+
+	Vatican City
+
+	Wallis and Futuna Islands
+
+	Zaire
+
+&quot;))]</value>
+   </webElementXpaths>
+</WebElementEntity>
